a9bb7f0e0dc0c4f9e6c6bd2bcdcb359c9e851cc4
zarref-semog <murilogf012@gmail.com>
2024-07-24 22:46:44 -0400

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//  import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import HomeScreen from './src/screens/home';
+import AboutScreen from './src/screens/about';
+
+const Tab = createBottomTabNavigator();
+//  const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='About' component={AboutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//  function App() {
+//      return (
+//      <NavigationContainer>
+//          <Stack.Navigator>
+//          <Stack.Screen name='Home' component={HomeScreen} />
+//          <Stack.Screen name='About' component={AboutScreen} />
+//          </Stack.Navigator>
+//      </NavigationContainer>
+//      );
+//  }
+
+export default App;
---
+++ src/screens/home.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+ import { View, Text, Button } from 'react-native';
+
+ function HomeScreen({navigation}) {
+     return (
+     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Home Screen</Text>
+         <Button title='Ir para About' onPress={() => navigation.navigate('About')} />
+     </View>
+     );
+ }
+
+ export default HomeScreen;
