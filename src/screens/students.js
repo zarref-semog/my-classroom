e940c3c86dc763b84636cd180fde8cbfd74d7bdc
zarref-semog <murilogf012@gmail.com>
2024-07-25 21:42:27 -0400

    adding list of classes and modal

---
+++ src/screens/students.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function StudentsScreen() {
+    return (
+        <View>
+            <Text>Alunos</Text>
+        </View>
+    );
+}
